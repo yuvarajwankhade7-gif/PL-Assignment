2092953c64351560016fa936c3e2a9153289a239
yuvarajwankhade7-gif <yuvarajwankhade7@gmail.com>
2026-03-28 21:51:26 +0530

    Add files via upload

---
+++ PL-Assignment-8.py
@@ -0,0 +1,30 @@
+class Vehicle:
+    def move(self):
+        print("Vehicle is moving")
+
+
+# ---------------- Subclass: Car ----------------
+class Car(Vehicle):
+    def move(self):
+        print("Driving on the road")
+
+
+# ---------------- Subclass: Bicycle ----------------
+class Bicycle(Vehicle):
+    def move(self):
+        print("Pedaling on the road")
+
+
+# ---------------- Main Program ----------------
+if __name__ == "__main__":
+
+    car = Car()
+    bicycle = Bicycle()
+
+    # Demonstrating Polymorphism
+    vehicles = [car, bicycle]
+
+    print("Demonstrating Polymorphism:\n")
+
+    for vehicle in vehicles:
+        vehicle.move()
